931e13f44a0d1106a5f80f7b35c0879ec9de1025
Davi <davicairo.dev@gmil.com>
2025-12-18 20:42:45 -0300

    style(design-system): padronização de formulários e estados visuais

--- PilotMaster.Infrastructure/obj/Debug/net8.0/PilotMaster.Infrastructure.AssemblyInfo.cs
+++ PilotMaster.Infrastructure/obj/Debug/net8.0/PilotMaster.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PilotMaster.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e36e6d28c431626779844ed27c58612d550835f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43e2a2b705be82c50d01fef0834128cf0e9518cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PilotMaster.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PilotMaster.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- pilotmaster-frontend/src/pages/ShipForm.jsx
+++ pilotmaster-frontend/src/pages/ShipForm.jsx
@@ -70,6 +70,8 @@ export default function ShipForm() {
               />
             </div>
 
+            
+
             <div className="form-field">
               <label>GRT</label>
               <input
